c9ddf51a9e71a07101811de16fccc6e2f8ebe9e1
Ev Kontsevoy <ev@mailgunhq.com>
2011-06-21 17:31:09 -0700

    hi

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,9 +1,11 @@
 class Notifier < ActionMailer::Base
   default :from => "from@example.com"
 
-  def welcome(to=nil)
-    mail :to => to do |format|
-      format.text 
+  def welcome(p)
+    @params = p
+    mail :to => p['recipient'],
+         :subject => p['subject'] do |format|
+      format.text(p)
     end
   end
 end
